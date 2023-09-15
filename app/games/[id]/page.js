4b54603f5078ecb5b8f7f82876f4329601fe069b
Pedro <pedrito2992@gmail.com>
2023-09-15 19:36:32 +0200

    Author name issues solved

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -2,12 +2,14 @@ export default function Page({ params }) {
   const id = params.id;
   const jsonData = require(`../../../data/${id}.json`);
 
+  const { publisher, author } = jsonData;
+  const company = publisher || author || [{ name: "Unknown" }];
+
   return (
     <>
       <img src={jsonData.image}></img>
       <h1>
-        This is the {jsonData.name} page, the game is made by{" "}
-        {jsonData.publisher[0].name}
+        This is the {jsonData.name} page, the game is made by {company[0].name}
       </h1>
     </>
   );
