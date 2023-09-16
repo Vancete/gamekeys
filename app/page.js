def5063f90ce6f54338783abb0bc2d9fc3f34ee2
Vancete <vancete07@gmail.com>
2023-09-16 12:47:28 +0200

    Created main banner with some fancy animations (WIP)

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
-import { GameCard } from "@/src/components/GameCard/GameCard";
 import { Header } from "@/src/components/Header/Header";
+import { MainBanner } from "@/src/components/MainBanner/MainBanner";
+import { GameCard } from "@/src/components/GameCard/GameCard";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <MainBanner />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <GameCard />
       </main>
---
+++ src/components/MainBanner/MainBanner.js
@@ -0,0 +1,57 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import "./MainBanner.scss";
+
+export const MainBanner = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      const mouseX = e.pageX;
+      const mouseY = e.pageY;
+
+      const xPercentage = (mouseX / windowWidth) * 100;
+      const yPercentage = (mouseY / windowHeight) * 100;
+
+      setMousePosition({ x: xPercentage, y: yPercentage });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div className="main-banner">
+      <h1>
+        <span>
+          The best <b>games</b>
+        </span>
+        <span>
+          at best <b>prices</b>
+        </span>
+      </h1>
+      <img
+        className="back"
+        src="main-banner-back.png"
+        style={{
+          right: `${-mousePosition.x / 40}%`,
+          bottom: `${-mousePosition.y / 40}%`,
+        }}
+      ></img>
+      <img
+        className="front"
+        src="main-banner-front.png"
+        style={{
+          right: `${mousePosition.x / 20}%`,
+          top: `${-mousePosition.y / 20}%`,
+        }}
+      ></img>
+    </div>
+  );
+};
