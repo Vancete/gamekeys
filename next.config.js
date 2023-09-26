ecede5026513929f6a0227c9a054e1e0b33b1387
Vancete <vancete07@gmail.com>
2023-09-26 16:18:16 +0200

    Updated next config to solve CSS and build error problems.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
+const path = require('node:path')
+
 /** @type {import('next').NextConfig} */
-
-const path = require('path')
-
 const nextConfig = {
     output: 'export',
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
+    images: {
+        unoptimized: true,
+    },
+    webpack(config) {
+        //  Source: https://github.com/vercel/next.js/discussions/24013#discussioncomment-3787335
+        config.module.rules.forEach((rule) => {
+            const { oneOf } = rule
+            if (oneOf) {
+                oneOf.forEach((one) => {
+                    if (!`${one.issuer?.and}`.includes('_app')) return
+                    one.issuer.and = [path.resolve(__dirname)]
+                })
+            }
+        })
+        return config
     },
 }
 
