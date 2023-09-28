97f1d81cc7a5a2a5c24b771414eefb6d25bbbef3
Vancete <vancete07@gmail.com>
2023-09-28 12:46:03 +0200

    Partial rollback for previous commit.

--- src/components/WishList/WishList.js
+++ src/components/WishList/WishList.js
@@ -1,16 +1,12 @@
 import './WishList.scss'
 
 import WishItem from '../WishItem/WishItem'
-import { SadIcon } from '../Icons/sad-icon'
 
 const WishList = ({ favs, setFavs, showWishList }) => {
     return (
         <div className={`wishlist ${showWishList ? 'show' : ''}`}>
             {favs.length < 1 ? (
-                <div className="empty">
-                    <SadIcon />
-                    <p>Your wish list is empty</p>
-                </div>
+                <div className="empty"></div>
             ) : (
                 <ul>
                     {favs.map((game) => (
