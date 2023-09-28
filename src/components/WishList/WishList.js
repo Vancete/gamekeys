53de561fdb864b4d6d5a063cc32b5c4b11ac6e2b
Vancete <vancete07@gmail.com>
2023-09-28 12:18:38 +0200

    Wishlist sad face. Some other style changes.

--- src/components/WishList/WishList.js
+++ src/components/WishList/WishList.js
@@ -1,24 +1,25 @@
-import "./WishList.scss"
+import './WishList.scss'
 
-import WishItem from "../WishItem/WishItem"
+import WishItem from '../WishItem/WishItem'
+import { SadIcon } from '../Icons/sad-icon'
 
-const WishList = ({favs, setFavs, showWishList}) => {
-
-  return (
-    <div className={`wishlist ${showWishList ? 'show' : ""}`}> 
-        { favs.length < 1 
-            ? <p>The list is empty</p>
-            : <ul>{ favs.map( game => (
-                <WishItem 
-                    key={game.title} 
-                    game={game} 
-                    favs={favs}
-                    setFavs={setFavs} 
-                />))}
-            </ul> 
-        }
-    </div>
-  )
+const WishList = ({ favs, setFavs, showWishList }) => {
+    return (
+        <div className={`wishlist ${showWishList ? 'show' : ''}`}>
+            {favs.length < 1 ? (
+                <div className="empty">
+                    <SadIcon />
+                    <p>Your wish list is empty</p>
+                </div>
+            ) : (
+                <ul>
+                    {favs.map((game) => (
+                        <WishItem key={game.title} game={game} favs={favs} setFavs={setFavs} />
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
 }
 
 export default WishList
