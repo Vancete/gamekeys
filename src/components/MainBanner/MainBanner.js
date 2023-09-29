6291c357e61fda70903567c3a1e1cb031222e798
Vancete <vancete07@gmail.com>
2023-09-29 14:14:48 +0200

    Responsive WIP.

--- src/components/MainBanner/MainBanner.js
+++ src/components/MainBanner/MainBanner.js
@@ -46,65 +46,69 @@ export const MainBanner = () => {
     return (
         <div className="main-banner">
             <div className="content">
-                <h1>
-                    <span>
-                        The best <b>games</b>
-                    </span>
-                    <span>
-                        at better <b>prices</b>
-                    </span>
-                </h1>
-                <h2>
-                    Buy the cheapest CD keys of your favourite
-                    <br />
-                    games at the cheapest prices on internet
-                </h2>
+                <div className="text">
+                    <h1>
+                        <span>
+                            The best <b>games</b>
+                        </span>
+                        <span>
+                            at better <b>prices</b>
+                        </span>
+                    </h1>
+                    <h2>
+                        Buy the cheapest CD keys of your favourite
+                        <br />
+                        games at the cheapest prices on internet
+                    </h2>
+                </div>
             </div>
-            <img
-                className="back"
-                src={`${bannerImages.back}.jpg`}
-                style={{
-                    right: `${-mousePosition.x * (bannerMovement.back.horizontalPercent / 100)}%`,
-                    bottom: `${-mousePosition.y * (bannerMovement.back.verticalPercent / 100)}%`,
-                }}
-            ></img>
-            <picture
-                className="front shadow"
-                style={{
-                    right: `${mousePosition.x * (bannerMovement.front.horizontalPercent / 100)}%`,
-                    top: `${-mousePosition.y * (bannerMovement.front.verticalPercent / 100)}%`,
-                    filter: 'blur(16px) brightness(0) opacity(0.35)',
-                    transform: `perspective(${bannerMovement.front.perspective}px) translateX(${
-                        bannerMovement.front.shadowHorizontalPercent -
-                        mousePosition.x * (bannerMovement.front.shadowHorizontalPercent / 100)
-                    }%) translateZ(-8px) rotateX(${
-                        mousePosition.y * (bannerMovement.front.verticalPerspectiveDegrees / 100)
-                    }deg) rotateY(${
-                        -bannerMovement.front.horizontalPerspectiveDegrees +
-                        mousePosition.x * (bannerMovement.front.horizontalPerspectiveDegrees / 100)
-                    }deg)`,
-                }}
-            >
-                <source type="image/webp" srcset={`${bannerImages.front}.webp`} />
-                <img src={`${bannerImages.front}.png`} alt="test" />
-            </picture>
-            <picture
-                className="front"
-                src="main-banner-front-cyb.webp"
-                style={{
-                    right: `${mousePosition.x * (bannerMovement.front.horizontalPercent / 100)}%`,
-                    top: `${-mousePosition.y * (bannerMovement.front.verticalPercent / 100)}%`,
-                    transform: `perspective(${bannerMovement.front.perspective}px) rotateX(${
-                        mousePosition.y * (bannerMovement.front.verticalPerspectiveDegrees / 100)
-                    }deg) rotateY(${
-                        -bannerMovement.front.horizontalPerspectiveDegrees +
-                        mousePosition.x * (bannerMovement.front.horizontalPerspectiveDegrees / 100)
-                    }deg)`,
-                }}
-            >
-                <source type="image/webp" srcset={`${bannerImages.front}.webp`} />
-                <img src={`${bannerImages.front}.png`} alt="test" />
-            </picture>
+            <div className="images">
+                <img
+                    className="back"
+                    src={`${bannerImages.back}.jpg`}
+                    style={{
+                        right: `${-mousePosition.x * (bannerMovement.back.horizontalPercent / 100)}%`,
+                        bottom: `${-mousePosition.y * (bannerMovement.back.verticalPercent / 100)}%`,
+                    }}
+                ></img>
+                <picture
+                    className="front shadow"
+                    style={{
+                        right: `${mousePosition.x * (bannerMovement.front.horizontalPercent / 100)}%`,
+                        top: `${-mousePosition.y * (bannerMovement.front.verticalPercent / 100)}%`,
+                        filter: 'blur(16px) brightness(0) opacity(0.35)',
+                        transform: `perspective(${bannerMovement.front.perspective}px) translateX(${
+                            bannerMovement.front.shadowHorizontalPercent -
+                            mousePosition.x * (bannerMovement.front.shadowHorizontalPercent / 100)
+                        }%) translateZ(-8px) rotateX(${
+                            mousePosition.y * (bannerMovement.front.verticalPerspectiveDegrees / 100)
+                        }deg) rotateY(${
+                            -bannerMovement.front.horizontalPerspectiveDegrees +
+                            mousePosition.x * (bannerMovement.front.horizontalPerspectiveDegrees / 100)
+                        }deg)`,
+                    }}
+                >
+                    <source type="image/webp" srcSet={`${bannerImages.front}.webp`} />
+                    <img src={`${bannerImages.front}.png`} alt="test" />
+                </picture>
+                <picture
+                    className="front"
+                    src="main-banner-front-cyb.webp"
+                    style={{
+                        right: `${mousePosition.x * (bannerMovement.front.horizontalPercent / 100)}%`,
+                        top: `${-mousePosition.y * (bannerMovement.front.verticalPercent / 100)}%`,
+                        transform: `perspective(${bannerMovement.front.perspective}px) rotateX(${
+                            mousePosition.y * (bannerMovement.front.verticalPerspectiveDegrees / 100)
+                        }deg) rotateY(${
+                            -bannerMovement.front.horizontalPerspectiveDegrees +
+                            mousePosition.x * (bannerMovement.front.horizontalPerspectiveDegrees / 100)
+                        }deg)`,
+                    }}
+                >
+                    <source type="image/webp" srcset={`${bannerImages.front}.webp`} />
+                    <img src={`${bannerImages.front}.png`} alt="test" />
+                </picture>
+            </div>
         </div>
     )
 }
